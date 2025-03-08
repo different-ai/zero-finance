762eae6f7a929ead0237459c685ea6cc21720ac3
Benjamin Shafii <ben@prologe.io>
2025-03-07 16:30:58 -0800

    delete old web

--- packages/web/lib/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-
-// biome-ignore lint: Forbidden non-null assertion.
-const client = postgres(process.env.POSTGRES_URL_2!);
-export const db = drizzle(client);
