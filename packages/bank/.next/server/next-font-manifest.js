8bea68b6b8b5e05c681d5736131264e224fe58af
Benjamin Shafii <ben@prologe.io>
2025-03-23 19:59:12 -0700

    [Cursor] Remove .next build directory from git tracking

--- packages/bank/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/benjaminshafii/git/hypr-v0/packages/bank/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
