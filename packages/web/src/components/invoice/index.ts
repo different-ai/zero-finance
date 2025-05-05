7ab5f42b1e029feb1bbf4287074248202a4f3098
Benjamin Shafii <ben@prologe.io>
2025-05-05 13:52:17 -0700

    chore: remove unused invoice, layout, and settings components

--- packages/web/src/components/invoice/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './invoice-chatbot';
-export * from './invoice-form';
-export * from './invoice-list-container';
-export * from './invoice-container';
-export * from './invoice-wrapper';
