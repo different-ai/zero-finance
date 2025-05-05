cc8486adaf2ecdbc8517a75b1b048f87fd924dfe
Benjamin Shafii <ben@prologe.io>
2025-05-05 13:50:26 -0700

    chore: remove unused types and invoice components

--- packages/web/src/types/address.ts
+++
@@ -1,9 +0,0 @@
-export interface Address {
-  'country-name'?: string;
-  'extended-address'?: string;
-  locality?: string;
-  'post-office-box'?: string;
-  'postal-code'?: string;
-  region?: string;
-  'street-address'?: string;
-} 
