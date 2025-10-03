58045e2a4099ab4dd4c76f0e60923b89a6e0952d
Benjamin Shafii <ben@prologe.io>
2025-10-03 16:15:13 -0700

    Remove unused test and demo pages

--- packages/web/src/app/dashboard/demo/page.tsx
+++
@@ -1,8 +0,0 @@
-import DemoPageWrapper from './demo-page-wrapper';
-
-// Force dynamic rendering to avoid pre-rendering issues with useSearchParams
-export const dynamic = 'force-dynamic';
-
-export default function DashboardDemoPage() {
-  return <DemoPageWrapper />;
-}
